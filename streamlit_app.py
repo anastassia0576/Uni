3d703944f902e4173b7b325aad07fa42bb313c8e
anastassia0576 <141334881+anastassia0576@users.noreply.github.com>
2024-12-03 02:19:59 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,9 +17,114 @@ geojson_data = {
             "properties": {"name": "Казахский агротехнический университет имени С. Сейфуллина, главный корпус"},
             "geometry": {"coordinates": [71.40919952047997, 51.18714511950674], "type": "Point"},
         },
-        # Добавьте остальные объекты из вашего GeoJSON...
+        {
+            "type": "Feature",
+      "properties": {"name": "Биологический корпус КазАТУ"},
+      "geometry": {
+        "coordinates": [
+          71.4094857538959,
+          51.18797538164955], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Корпус технического факультета КазАТУ"},
+      "geometry": {
+        "coordinates": [
+          71.41114324507092,
+          51.1877459137084], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Новый корпус технического факультета КазАТУ"},
+      "geometry": {
+        "coordinates": [
+          71.41172902508583,
+          51.18715346392423], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Корпус военной кафедры КазАТУ "},
+      "geometry": {
+        "coordinates": [
+          71.41081041551712,
+          51.18707001968042], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Склад КазАТУ "},
+      "geometry": {
+        "coordinates": [
+          71.41170239872159,
+          51.18659438460773], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Общежитие №7 КазАТУ "},
+      "geometry": {
+        "coordinates": [
+          71.41260769510728,
+          51.1863148424068], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Общежитие №2б КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.41294052466108,
+          51.18694068080447], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Общежитие №9 КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.4132400712582,
+          51.18633153154096], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Общежитие №4 КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.41429301517488,
+          51.18643214747047], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Корпус земельного факультета КазАту "},
+      "geometry": {
+        "coordinates": [
+         71.41529891243619,
+          51.18658181999851], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Общежитие №5 КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.41385674718038,
+          51.18641280852361], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Гараж КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.41107883675795,
+          51.186700066345196], "type": "Point"},
+        },
+        {
+            "type": "Feature",
+      "properties": {"name": "Корпус кафедры технической механики КазАТУ "},
+      "geometry": {
+        "coordinates": [
+         71.41207484233962,
+          51.186695146518645], "type": "Point"},
+        },
     ],
-}
+
+}# Добавьте остальные объекты из вашего GeoJSON...
+ 
 
 # Создаем карту Folium
 m = folium.Map(location=[51.186845, 71.41058], zoom_start=14)
