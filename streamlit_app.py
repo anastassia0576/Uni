5a00484d4f7fed20e361bba46e0949bbe85883b0
anastassia0576 <141334881+anastassia0576@users.noreply.github.com>
2024-12-03 02:16:02 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,6 @@ st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-import streamlit as st
-import requests
-
 import folium
 from folium.plugins import Search, MarkerCluster
 
